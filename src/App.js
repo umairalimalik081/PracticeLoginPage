bf42f05880de542b3e44ac835c0a06031d7c2606
umairalimalik081 <umairalimalik081@gmail.com>
2023-05-25 22:40:00 +0400

    useeffect hook added in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import LoginForm from './Components/LoginForm';
-import './App.css';
+import LoginForm from "./Components/LoginForm";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    console.log("hello");
+  }, []);
   return (
-    <div >
-     <LoginForm/>
+    <div>
+      <LoginForm />
     </div>
   );
 }
